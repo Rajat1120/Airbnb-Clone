d4ba9e90618f89f8e9560363af7b83e7c74828ca
Rajat1120 <rajat1120@example.com>
2024-03-01 00:21:26 +0530

    create scroll options

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import styles from "./input.css";
 import Header from "./Header/Header";
+import Options from "./Main/Options";
 export default function App() {
   return (
     <div className="">
       <Header></Header>
+      <Options></Options>
     </div>
   );
 }
