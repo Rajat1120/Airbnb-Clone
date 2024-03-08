7e442e574fe5f7aafef08f19d9f3e7285534e021
Rajat1120 <rajat1120@example.com>
2024-03-08 17:40:58 +0530

    create filter and display before taxes button

--- src/Main/FilterHome.js
+++ src/Main/FilterHome.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const FilterHome = () => {
   return (
-    <div className={"w-[30%]       rounded-lg overflow-hidden"}>
-      <button>Filter</button>
-      <button>Display total before taxes</button>
+    <div
+      className={
+        "w-[30%]  gap-x-[10px]     rounded-lg flex items-center justify-center overflow-hidden"
+      }
+    >
+      <button className="border-[1px] py-[7px] text-[12px] font-medium w-[92px] h-[48px] border-grey-dim rounded-lg flex items-center justify-center ">
+        Filters
+      </button>
+      <button className="border-[1px] py-[10px] px-[16px] text-[12px] font-medium w-[205px] h-[48px] border-grey-dim rounded-lg flex items-center justify-center ">
+        Display total before taxes
+      </button>
     </div>
   );
 };
