5468911ec5832405592d09c800260a967c45fded
Rajat kumar <rajat812678@gmail.com>
2024-03-08 16:37:53 +0530

    fixed - border-blur issue

--- src/Main/FilterHome.js
+++ src/Main/FilterHome.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FilterHome = () => {
   return (
-    <div className={"w-[30%]    border-blur    rounded-lg overflow-hidden"}>
+    <div className={"w-[30%]       rounded-lg overflow-hidden"}>
       <button>Filter</button>
       <button>Display total before taxes</button>
     </div>
