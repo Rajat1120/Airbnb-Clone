ec7e298c82b9fe31d14211670c7973d54a1cd1f0
Rajat kumar <rajat812678@gmail.com>
2024-05-08 23:20:38 +0530

    implement functionality to highlight filter btn on hover

--- src/Main/buttons/FilterHome.js
+++ src/Main/buttons/FilterHome.js
@@ -9,11 +9,11 @@ const FilterHome = () => {
         "w-[380px]  gap-x-[10px] mx-0   rounded-lg flex items-center justify-center "
       }
     >
-      <button className="border-[1px] py-[7px] text-[12px] font-medium w-[92px] h-[48px] border-grey-dim rounded-lg flex items-center gap-x-1 justify-center ">
+      <button className="border-[1px] hover:bg-gray-100 hover:border-black py-[7px] text-[12px] font-medium w-[92px] h-[48px] border-grey-dim rounded-lg flex items-center gap-x-1 justify-center ">
         <img src={filter_icon} className="transform scale-[80%]" alt="" />
         Filters
       </button>
-      <button className="border-[1px] py-[10px] px-[16px] text-[12px] font-medium w-[205px] h-[48px] border-grey-dim rounded-lg flex items-center justify-center ">
+      <button className="border-[1px]  hover:bg-gray-100 hover:border-black  py-[10px] px-[16px] text-[12px] font-medium w-[205px] h-[48px] border-grey-dim rounded-lg flex items-center justify-center ">
         Display total Taxes before
         {/* <Toggle></Toggle> */}
       </button>
