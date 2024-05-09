4b3e1b736b8ecca31dfb2ee084084a9c85b45790
Rajat kumar <rajat812678@gmail.com>
2024-05-09 15:36:49 +0530

    add toggle btn and made some border changes to filter btn

--- src/Main/buttons/FilterHome.js
+++ src/Main/buttons/FilterHome.js
@@ -6,16 +6,16 @@ const FilterHome = () => {
   return (
     <div
       className={
-        "w-[380px]  gap-x-[10px] mx-0   rounded-lg flex items-center justify-center "
+        "w-[420px]  gap-x-[10px] mx-0  px-10  rounded-lg flex items-center justify-around "
       }
     >
-      <button className="border-[1px] hover:bg-gray-100 hover:border-black py-[7px] text-[12px] font-medium w-[92px] h-[48px] border-grey-dim rounded-lg flex items-center gap-x-1 justify-center ">
+      <button className="border-[1px] hover:bg-gray-100 hover:border-black py-[7px] text-[12px] font-medium w-[92px] h-[48px] border-grey-dim rounded-xl flex items-center gap-x-1 justify-center ">
         <img src={filter_icon} className="transform scale-[80%]" alt="" />
         Filters
       </button>
-      <button className="border-[1px]  hover:bg-gray-100 hover:border-black  py-[10px] px-[16px] text-[12px] font-medium w-[205px] h-[48px] border-grey-dim rounded-lg flex items-center justify-center ">
-        Display total Taxes before
-        {/* <Toggle></Toggle> */}
+      <button className="border-[1px]  hover:bg-gray-100 hover:border-black  py-[10px] px-[16px] text-[11px] font-medium w-[222px] h-[48px] border-grey-dim rounded-xl flex justify-between  items-center ">
+        <p>Display total before taxes</p>
+        <Toggle></Toggle>
       </button>
     </div>
   );
