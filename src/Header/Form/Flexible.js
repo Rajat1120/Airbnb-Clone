b0300409af7cfd5f0207595e37fa8c4b09d4ad34
Rajat kumar <rajat812678@gmail.com>
2024-07-18 17:00:55 +0530

    add left right scroll button for months in flexible component

--- src/Header/Form/Flexible.js
+++ src/Header/Form/Flexible.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Modal from "../../Modals/Modal";
 import CheckInOption from "./DatesOption";
 import monthSvg from "../../data/Icons svg/month.svg"
@@ -10,7 +10,9 @@ import { useSelector } from "react-redux";
 const Flexible = ({ modalRef, handleInputField, flexibleRef }) => {
    const [scrollPosition, setScrollPosition] = useState(0);
   const curInput = useSelector((store) => store.form.curSelectInput);
-
+  let monthRef = useRef()
+  let rightScrollBtnRef = useRef()
+  let leftScrollBtnRef = useRef()
   function getNext12Months() {
     const months = [];
     const currentDate = new Date();
@@ -28,9 +30,75 @@ const Flexible = ({ modalRef, handleInputField, flexibleRef }) => {
   let result = getNext12Months()
 
 
-    let btnLeftClassName = ` absolute z-30   top-[25%] left-0 h-9 hidden  w-9  z-100 bg-white hover:scale-110 hover:drop-shadow-md  rounded-[50%] border-[1px] border-grey-dim`;
 
-  let btnRightClassName = `absolute  top-[25%] z-30 right-2 h-9  w-9 border-grey-dim bg-white hover:scale-110 hover:drop-shadow-md   rounded-[50%] border-[1px]`;
+    useEffect(() => {
+    const month = monthRef.current;
+    const handleScroll = () => {
+      if (month) {
+        setScrollPosition(month.scrollLeft);
+      }
+    };
+
+    if (month) {
+      month.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (month) {
+        month.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+    useEffect(() => {
+    let leftScrollButtonRef = leftScrollBtnRef.current;
+    function handleScrollLeftBtn() {
+      const newPosition = scrollPosition - 760;
+      monthRef.current.scrollTo({
+        left: newPosition,
+        behavior: "smooth",
+      });
+      setScrollPosition(newPosition);
+    }
+
+    if (leftScrollButtonRef) {
+      leftScrollButtonRef.addEventListener("click", handleScrollLeftBtn);
+    }
+
+    return () => {
+      if (leftScrollButtonRef) {
+        leftScrollButtonRef.removeEventListener("click", handleScrollLeftBtn);
+      }
+    };
+  }, [scrollPosition]);
+
+
+   useEffect(() => {
+    let rightScrollBtn = rightScrollBtnRef.current;
+    function handleScrollRightBtn() {
+      const newPosition = scrollPosition + 760;
+      monthRef.current.scrollTo({
+        left: newPosition,
+        behavior: "smooth",
+      });
+      setScrollPosition(newPosition);
+    }
+
+    if (rightScrollBtn) {
+      rightScrollBtn.addEventListener("click", handleScrollRightBtn);
+    }
+
+    return () => {
+      if (rightScrollBtn) {
+        rightScrollBtn.removeEventListener("click", handleScrollRightBtn);
+      }
+    };
+  }, [scrollPosition]);
+
+
+    let btnLeftClassName = `absolute ${scrollPosition < 1 ? "hidden" : "flex-center" } top-[72%] left-5 h-8 w-8  bg-white z-30 rounded-[50%] hover:scale-110 hidden hover:drop-shadow-md border-[1px] border-grey-dim-light `;
+
+   let btnRightClassName = `absolute  ${scrollPosition > 758 ? "hidden" : "flex-center" } top-[72%] right-5 h-8 w-8  bg-white z-30 rounded-[50%] hover:scale-110 hover:drop-shadow-md border-[1px] border-grey-dim-light `;
 
   return (
     <Modal>
@@ -54,40 +122,46 @@ const Flexible = ({ modalRef, handleInputField, flexibleRef }) => {
         </div>
       </Modal.Open>
       <Modal.Window modalRef={modalRef} name="flexible">
-        <div className="flex flex-col w-full justify-center items-center">
+        <div className="flex relative flex-col  w-full justify-center items-center">
           <CheckInOption></CheckInOption>
           <div className="w-full mt-4 flex-col flex items-center">
-            <span className="text-xl">How long would you like to stay?</span>
+            <span className="text-lg font-medium">How long would you like to stay?</span>
             <div className="flex items-center my-5 gap-x-2">
-              <span className="py-2 font-extralight px-4 border-[1px] border-grey-light rounded-full">Weekend</span>
-              <span className="py-2 font-extralight px-4 border-[1px] border-grey-light rounded-full">Week</span>
-              <span className="py-2 font-extralight px-4 border-[1px] border-grey-light rounded-full">Month</span>
+              <span className="py-2 text-sm  font-extralight px-4 border-[1px] border-grey-light rounded-full">Weekend</span>
+              <span className="py-2 text-sm font-extralight px-4 border-[1px] border-grey-light rounded-full">Week</span>
+              <span className="py-2 text-sm font-extralight px-4 border-[1px] border-grey-light rounded-full">Month</span>
             </div>
           </div>
-          <div className="w-full max-w-[65rem] mb-10 flex flex-col items-center">
-            <span className="text-xl mt-10 mb-6" >When do you want to go?</span>
-            <div className="w-full overflow-x-auto">
-              <div className="inline-flex gap-x-2 pb-4">
+          <div className="w-full  max-w-[65rem] mb-12 flex flex-col items-center">
+            <span className="text-lg font-medium mt-10 mb-6" >When do you want to go?</span>
+            <div ref={monthRef} className="w-full  overflow-x-auto">
+              <div  className="inline-flex gap-x-2 pb-4">
                 {result.map((item, index) => (
-                  <div key={index} className={`flex-shrink-0 rounded-2xl flex-center flex-col ${index === 0 ? "ml-9" : ""}  w-[7.5rem] border-[1px] border-grey-dim h-[8.5rem]`}>
+                  <div key={index} className={`flex-shrink-0 rounded-2xl flex-center flex-col ${index === 0 ? "ml-10" : ""} ${index === 11 ? "mr-10" : ""}  w-[7.5rem] border-[1px] border-grey-dim hover:border-black h-[8.5rem]`}>
                     <div  className="" ><img className="w-10 h-10"  src={monthSvg} alt="" /></div>
-                   <span>
+                   <span className="text-sm font-medium"  > 
 
                      {item.month} 
                    </span>
-                   <span>
+                   <span className="text-xs font-extralight"  >
 
                    {item.year}
                    </span>
                   </div>
                 ))}
               </div>
-              <button  className={btnLeftClassName}>
+              <button ref={leftScrollBtnRef}  className={btnLeftClassName}>
               <img src={arrow_left} className="h-6 " alt="" />
             </button>
-             <button  className={btnRightClassName}>
+             {scrollPosition > 0 && (
+              <div className="w-12 h-40 absolute   top-[60%] left-0 bg-white border-r-[0.8rem] border-white "></div>
+            )}
+             <button ref = {rightScrollBtnRef}  className={btnRightClassName}>
               <img src={arrow_right} className="h-6 " alt="" />
             </button>
+             {scrollPosition < 758 && (
+              <div className="w-12 h-40 absolute   top-[60%] right-0 bg-white border-r-[0.8rem] border-white "></div>
+            )}
             </div>
           </div>
         </div>
