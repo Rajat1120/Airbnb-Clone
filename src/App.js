5b1961a2999f60a7e5679c06036201779986d493
Rajat kumar <rajat812678@gmail.com>
2024-07-23 18:13:19 +0530

    Add React Router DOM for client-side routing

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
-import styles from "./input.css";
-import Header from "./Header/Header";
-import Options from "./Main/Options";
-import Toggle from "./Main/Toggle";
-import House from "./Main/House";
-import { useRef, useState } from "react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
-import { useSelector } from "react-redux";
-import { minutesInDay } from "date-fns/constants";
+import Home from "./Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+]);
 
 export default function App() {
-  const startScroll = useSelector((store) => store.app.startScroll);
-  const minimize = useSelector((store) => store.app.minimize);
-  let headerRef = useRef();
-
-  let animateHeaderClass1 = minimize ? "animate-expand" : "h-[5rem]";
-
-  let animateHeaderClass2 = minimize ? "animate-collapse" : "h-[11rem]";
-
-  return (
-    <div className="flex-center relative">
-      <div
-        ref={headerRef}
-        id="header"
-        className={` fixed ${
-          minimize ? "z-50" : "z-10"
-        }  transition-all duration-[0.3s] ease-in-out ${
-          !startScroll ? `${animateHeaderClass1}` : `${animateHeaderClass2}`
-        } bg-white   w-full flex items-start justify-center top-0 `}
-      >
-        <Header headerRef={headerRef}></Header>
-      </div>
-
-      <div
-        className={`  transition-all duration-[0.3s] ease-in-out flex-center  fixed z-10 w-full  bg-white  ${
-          !startScroll ? "-translate-y-[5.9rem] shadow-md" : ""
-        }   top-[10.5rem]  flex-center  `}
-      >
-        <Options></Options>
-      </div>
-
-      <div className="mt-[12rem]   flex justify-center items-center ">
-        <House></House>
-      </div>
-
-      <SpeedInsights></SpeedInsights>
-    </div>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
---
+++ src/Home.js
@@ -0,0 +1,48 @@
+import Header from "./Header/Header";
+import Options from "./Main/Options";
+import styles from "./input.css";
+
+import House from "./Main/House";
+import { useRef } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { useSelector } from "react-redux";
+
+export default function Home() {
+  const startScroll = useSelector((store) => store.app.startScroll);
+  const minimize = useSelector((store) => store.app.minimize);
+  let headerRef = useRef();
+
+  let animateHeaderClass1 = minimize ? "animate-expand" : "h-[5rem]";
+
+  let animateHeaderClass2 = minimize ? "animate-collapse" : "h-[11rem]";
+
+  return (
+    <div className="flex-center relative">
+      <div
+        ref={headerRef}
+        id="header"
+        className={` fixed ${
+          minimize ? "z-50" : "z-10"
+        }  transition-all duration-[0.3s] ease-in-out ${
+          !startScroll ? `${animateHeaderClass1}` : `${animateHeaderClass2}`
+        } bg-white   w-full flex items-start justify-center top-0 `}
+      >
+        <Header headerRef={headerRef}></Header>
+      </div>
+
+      <div
+        className={`  transition-all duration-[0.3s] ease-in-out flex-center  fixed z-10 w-full  bg-white  ${
+          !startScroll ? "-translate-y-[5.9rem] shadow-md" : ""
+        }   top-[10.5rem]  flex-center  `}
+      >
+        <Options></Options>
+      </div>
+
+      <div className="mt-[12rem]   flex justify-center items-center ">
+        <House></House>
+      </div>
+
+      <SpeedInsights></SpeedInsights>
+    </div>
+  );
+}
