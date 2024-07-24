274c454870a8b9cb2cb409a95422184772a87b56
Rajat kumar <rajat812678@gmail.com>
2024-07-24 09:42:21 +0530

    Add title, share and save button on the house details page

--- src/House-detail/HouseDetail.js
+++ src/House-detail/HouseDetail.js
@@ -18,17 +18,29 @@ const HouseDetail = () => {
 
   let animateHeaderClass2 = minimize ? "animate-collapse" : "h-[11rem]";
   return (
-    <div className="flex-center relative">
+    <div className="">
       <div
         ref={headerRef}
         id="header"
-        className={` fixed ${
+        className={`  ${
           minimize ? "z-50" : "z-10"
         }  transition-all duration-[0.3s] ease-in-out ${
           !startScroll ? `${animateHeaderClass1}` : `${animateHeaderClass2}`
-        } bg-white   w-full flex items-start justify-center top-0 `}
+        } bg-white   w-full flex items-start justify-center  `}
       >
         <Header headerRef={headerRef}></Header>
+      </div>
+      <div>
+        <div className="w-[calc(100%-20rem)] flex justify-between mx-auto">
+          <h1 className="pt-6 text-[27px] font-[460]">
+            Alsisar Haveli - A Heritage Home
+          </h1>
+          <div className="pt-6 flex justify-between w-[7rem]">
+            <span className="underline text-sm font-medium">Share</span>
+            <span className="underline text-sm font-medium">save</span>
+          </div>
+        </div>
+        <div></div>
       </div>
     </div>
   );
