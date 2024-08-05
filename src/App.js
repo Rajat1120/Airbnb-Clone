7cf102c9e7be197425ca4d4af6dfbe61ecd082f3
Rajat kumar <rajat812678@gmail.com>
2024-08-05 14:54:42 +0530

    add vercel analytics

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HouseDetail from "./House-detail/HouseDetail";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
+import { Analytics } from "@vercel/analytics/react";
 const queryClient = new QueryClient({});
 
 const router = createBrowserRouter([
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Analytics />
       <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
       <RouterProvider router={router}></RouterProvider>;
     </QueryClientProvider>
