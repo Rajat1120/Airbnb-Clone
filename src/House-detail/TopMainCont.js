f71d70eb1a7fb911c05c6110f412904e059792f9
Rajat kumar <rajat812678@gmail.com>
2024-08-06 12:44:00 +0530

    Fix: Prevent layout shifts of images by adding a fixed number of grid areas

--- src/House-detail/TopMainCont.js
+++ src/House-detail/TopMainCont.js
@@ -10,6 +10,15 @@ const TopMainCont = () => {
   const isLoading = useSelector((store) => store.houseDetail.isLoading);
   const houseInfo = useSelector((store) => store.houseDetail.houseInfo[id]);
 
+  // Pre-define the grid layout
+  const gridLayout = [
+    "grid-area-image1",
+    "grid-area-image2",
+    "grid-area-image3",
+    "grid-area-image4",
+    "grid-area-image5",
+  ];
+
   return (
     <div className="flex-center flex-col">
       <div className="w-[calc(100%-20rem)] flex justify-between mx-auto">
@@ -18,7 +27,7 @@ const TopMainCont = () => {
         </h1>
         <div className="pt-6 flex justify-between w-[10rem]">
           <span className="underline w-[5.2rem] rounded-md h-8 hover:bg-shadow-gray-light text-sm font-medium justify-center hover:cursor-pointer gap-2 items-center flex">
-            <img className="w-[1.2rem] h-[1.2rem] pt-1" src={share} alt="" />{" "}
+            <img className="w-[1.2rem] h-[1.2rem] pt-1" src={share} alt="" />
             <span className="h-[1.2rem]">Share</span>
           </span>
           <span className="underline w-[4.8rem] rounded-md h-8 hover:bg-shadow-gray-light text-sm font-medium justify-center hover:cursor-pointer gap-2 items-center flex">
@@ -29,25 +38,33 @@ const TopMainCont = () => {
       </div>
       <div className="w-[calc(100%-20rem)] px-auto">
         <div className="pt-6">
-          <div className="grid-areas rounded-xl overflow-hidden">
-            {[1, 2, 3, 4, 5].map((index) => (
-              <div key={index} className={`grid-area-image${index} relative`}>
-                {houseInfo?.images[index - 1] && (
-                  <img
-                    src={houseInfo.images[index - 1]}
-                    alt={`House img ${index}`}
-                    className="w-full h-full object-cover"
-                  />
-                )}
-                {index === 5 && (
-                  <div className="flex-center cursor-pointer w-[10rem] h-8 bg-white absolute bottom-5 right-5 gap-x-2 rounded-lg border-[1px] border-black">
-                    <img src={dots} className="!w-4 !h-4" alt="" />
-                    <span className="text-sm font-medium">Show all photos</span>
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
+          {isLoading ? (
+            <div className="h-[25rem] w-full">Loading...</div>
+          ) : (
+            <div className="grid-areas rounded-xl overflow-hidden">
+              {gridLayout.map((gridArea, index) => (
+                <div key={index} className={gridArea}>
+                  {houseInfo?.images &&
+                    !isLoading &&
+                    houseInfo.images[index] && (
+                      <img
+                        src={houseInfo.images[index]}
+                        alt={`House img ${index + 1}`}
+                        className="w-full h-full object-cover"
+                      />
+                    )}
+                  {index === 4 && (
+                    <div className="flex-center cursor-pointer w-[10rem] h-8 bg-white absolute bottom-5 right-5 gap-x-2 rounded-lg border-[1px] border-black">
+                      <img src={dots} className="!w-4 !h-4" alt="" />
+                      <span className="text-sm font-medium">
+                        Show all photos
+                      </span>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
