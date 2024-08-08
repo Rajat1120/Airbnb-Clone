70ec0efea9cc297a5c8e4c119e42e0ca432712d6
Rajat kumar <rajat812678@gmail.com>
2024-08-08 13:04:28 +0530

    Refactored options scroll logic to simplify code

--- src/Main/Options.js
+++ src/Main/Options.js
@@ -4,15 +4,14 @@ import arrow_left from "./../data/Icons svg/arrow-left.svg";
 import arrow_right from "./../data/Icons svg/arrow-right.svg";
 import { useDispatch, useSelector } from "react-redux";
 import optionImgs from "../OptionsImgs";
-
 import { setSelectedIcon } from "./AppSlice";
 import { getRooms } from "../Services/apiRooms";
 import { useQuery } from "@tanstack/react-query";
 
 const Options = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
   const [uniqueFilters, setUniqueFilters] = useState([]);
-  const [containerScrollWidth, setContainerScrollWidth] = useState(0);
+  const [isAtStart, setIsAtStart] = useState(true);
+  const [isAtEnd, setIsAtEnd] = useState(false);
   const selectedCountry = useSelector((store) => store.app.selectedCountry);
   const city = useSelector((store) => store.app.city);
   const minimize = useSelector((store) => store.app.minimize);
@@ -24,19 +23,16 @@ const Options = () => {
   const { isLoading, data, error } = useQuery({
     queryKey: ["country", selectedCountry],
     queryFn: () => getRooms(selectedCountry, city),
-    enabled: !!selectedCountry, // This makes sure the query only runs when selectedIcon is set
+    enabled: !!selectedCountry,
   });
 
   useEffect(() => {
     if (data) {
-      // Function to find unique filter values
       const findUniqueFilters = (data) => {
         const filters = data.map((item) => item.filter);
         const uniqueFilters = [...new Set(filters)];
         return uniqueFilters;
       };
-
-      // Update the state with unique filters
       const uniqueFilterValues = findUniqueFilters(data);
       setUniqueFilters(uniqueFilterValues);
     }
@@ -45,113 +41,73 @@ const Options = () => {
   const dispatch = useDispatch();
 
   const optionsRef = useRef(null);
-  const rightScrollBtnRef = useRef(null);
-  const leftScrollBtnRef = useRef(null);
-  const totalScrollWidth = optionsRef?.current?.scrollWidth - 935;
+  const itemRefs = useRef([]);
 
-  useEffect(() => {
-    if (optionsRef?.current) {
-      setContainerScrollWidth(Math.abs(optionsRef?.current?.scrollWidth / 8));
+  const handleScroll = () => {
+    const container = optionsRef.current;
+    if (container) {
+      const { scrollLeft, scrollWidth, clientWidth } = container;
+
+      setIsAtStart(scrollLeft < itemRefs?.current[0]?.offsetWidth);
+      setIsAtEnd(
+        Math.abs(scrollWidth - clientWidth - scrollLeft) <
+          itemRefs?.current[itemRefs.current.length - 1].offsetWidth
+      );
     }
-  }, []);
+  };
 
   useEffect(() => {
     const optionRef = optionsRef?.current;
-
-    const handleScroll = () => {
-      if (optionRef) {
-        setScrollPosition(optionRef.scrollLeft);
-      }
-    };
-
     if (optionRef) {
       optionRef.addEventListener("scroll", handleScroll);
     }
-
     return () => {
       if (optionRef) {
         optionRef.removeEventListener("scroll", handleScroll);
       }
     };
-  }, [options]);
+  }, []);
 
-  useEffect(() => {
-    let rightScrollBtn = rightScrollBtnRef.current;
-    function handleScrollRightBtn() {
-      const newPosition = scrollPosition + containerScrollWidth;
-      optionsRef.current.scrollTo({
-        left: newPosition,
-        behavior: "smooth",
-      });
-      setScrollPosition(newPosition);
+  const handleScrollBtn = (direction) => {
+    const container = optionsRef.current;
+    if (container) {
+      const itemWidth = itemRefs.current[0]?.offsetWidth * 8 || 0;
+      const scrollAmount = direction === "right" ? itemWidth : -itemWidth;
+      container.scrollBy({ left: scrollAmount, behavior: "smooth" });
     }
-
-    if (rightScrollBtn) {
-      rightScrollBtn.addEventListener("click", handleScrollRightBtn);
-    }
-
-    return () => {
-      if (rightScrollBtn) {
-        rightScrollBtn.removeEventListener("click", handleScrollRightBtn);
-      }
-    };
-  }, [scrollPosition, containerScrollWidth]);
-
-  useEffect(() => {
-    let leftScrollButtonRef = leftScrollBtnRef.current;
-    function handleScrollLeftBtn() {
-      const newPosition = scrollPosition - containerScrollWidth;
-      optionsRef.current.scrollTo({
-        left: newPosition,
-        behavior: "smooth",
-      });
-      setScrollPosition(newPosition);
-    }
-
-    if (leftScrollButtonRef) {
-      leftScrollButtonRef.addEventListener("click", handleScrollLeftBtn);
-    }
-
-    return () => {
-      if (leftScrollButtonRef) {
-        leftScrollButtonRef.removeEventListener("click", handleScrollLeftBtn);
-      }
-    };
-  }, [scrollPosition, containerScrollWidth]);
-
-  let btnLeftClassName = `absolute z-30 ${
-    scrollPosition < 65 ? "hidden" : "flex-center"
-  } top-[30%] left-1 h-9 hidden w-9 z-100 bg-white hover:scale-110 hover:drop-shadow-md rounded-[50%] border-[1px] border-grey-dim`;
-
-  let btnRightClassName = `absolute ${
-    scrollPosition > totalScrollWidth ? "hidden" : "flex-center"
-  } top-[30%] z-50 right-4 h-9 w-9 border-grey-dim bg-white hover:scale-110 hover:drop-shadow-md rounded-[50%] border-[1px]`;
+  };
 
   return (
     <div
       className={`bg-white ${
         minimize ? "hidden" : ""
-      } z-10 justify-self-center w-[calc(100%-10rem)] mx-auto`}
+      } z-10 justify-self-center w-[calc(100%-10rem)]  mx-auto`}
     >
-      <div className={`h-[5rem] py-6 flex w-full items-center justify-between`}>
-        <div
-          className={
-            "w-full flex relative items-center overflow-scroll rounded-lg"
-          }
-        >
+      <div
+        className={`h-[5rem] py-6 flex w-full relative items-center justify-between space-x-10`}
+      >
+        <div className="w-full flex  items-center overflow-hidden rounded-lg">
           <div className="flex items-center justify-start inset-shadow w-full">
             <div
               id="options"
               ref={optionsRef}
-              className="flex items-center space-x-9 justify-start h-24 w-[56rem] overflow-x-auto"
+              className="flex items-center space-x-10 justify-start h-24 w-full overflow-x-auto scroll-smooth"
+              style={{
+                scrollBehavior: "smooth",
+              }}
             >
               {options.map((item, i) => (
                 <div
+                  ref={(el) => (itemRefs.current[i] = el)}
                   onClick={() => dispatch(setSelectedIcon(item.iconName))}
                   key={i}
                   className={`opacity-75 hover:opacity-100 cursor-pointer flex-center mr-0 ${
                     i === 0 ? "pl-2" : ""
-                  } h-16 my-[12px] border-b-2 border-white py-[4px] hover:border-grey-light-50 w-full`}
+                  } h-16 my-[12px] border-b-2 border-white py-[4px] hover:border-grey-light-50`}
+                  style={{
+                    scrollSnapAlign: "start",
+                    flexShrink: 0,
+                  }}
                 >
                   <div className="flex-col space-y-2 h-full items-center justify-center flex">
                     <img
@@ -166,17 +122,31 @@ const Options = () => {
                 </div>
               ))}
             </div>
-            <button ref={leftScrollBtnRef} className={btnLeftClassName}>
-              <img src={arrow_left} className="h-6" alt="" />
-            </button>
-            {scrollPosition > 65 && (
-              <div className="w-12 h-20 absolute z-10 left-0 bg-white border-r-[0.8rem] border-white"></div>
+            {!isAtStart && (
+              <>
+                <div className="absolute -left-1    w-10 h-20 bg-white">
+                  <div className="absolute left-4 w-full h-full bg-white blur-right"></div>
+                </div>
+                <button
+                  onClick={() => handleScrollBtn("left")}
+                  className="absolute z-30 flex-center     top-[30%] left-0 h-9 w-9 bg-white hover:scale-110 hover:drop-shadow-md rounded-[50%]  border-[1px] border-grey-dim"
+                >
+                  <img src={arrow_left} className="h-6" alt="Scroll left" />
+                </button>
+              </>
             )}
-            <button ref={rightScrollBtnRef} className={btnRightClassName}>
-              <img src={arrow_right} className="h-6" alt="" />
-            </button>
-            {scrollPosition < totalScrollWidth && (
-              <div className="w-12 h-16 absolute z-10 right-0 bg-white border-r-[0.8rem] border-white"></div>
+            {!isAtEnd && (
+              <>
+                <div className="absolute right-[21.7rem]  w-10 h-20 bg-white">
+                  <div className="absolute -left-4 w-full h-full bg-white blur-left"></div>
+                </div>
+                <button
+                  onClick={() => handleScrollBtn("right")}
+                  className="absolute flex-center top-[30%] z-50 right-[22rem] h-9 w-9 border-grey-dim bg-white hover:scale-110 hover:drop-shadow-md rounded-[50%] border-[1px]"
+                >
+                  <img src={arrow_right} className="h-6" alt="Scroll right" />
+                </button>
+              </>
             )}
           </div>
         </div>
