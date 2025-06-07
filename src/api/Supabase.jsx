e48722434174788bfc927df5a55ab689de28ad11
Rajat1120 <rajat812678@gmail.com>
2025-06-08 00:32:01 +0530

    add .env file

--- src/api/Supabase.jsx
+++ src/api/Supabase.jsx
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://xailnuaqpfvjojptkpgh.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhaWxudWFxcGZ2am9qcHRrcGdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI1MDYwNzQsImV4cCI6MjAzODA4MjA3NH0.ku6paLHg6HCMyGXwmd7XSW8Gp_tjyDrux046qOTxjxg";
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("Supabase Key:", import.meta.env.VITE_SUPABASE_KEY);
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+);
 
 export default supabase;
